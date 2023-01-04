0e95deef0046e01d2a5855bfa0601567f1438cf3
paquitto99 <frajavmarmol@gmail.com>
2023-01-04 11:47:46 +0100

    mejoras en frontend panel

--- admin/includes/menubar.php
+++ admin/includes/menubar.php
@@ -4,7 +4,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="img-circle" alt="User Image">
+          <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/yo.jpg'; ?>" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p><?php echo $user['firstname'].' '.$user['lastname']; ?></p>
